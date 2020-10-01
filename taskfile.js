13f7b41a1973158d581d14ea6d237a5385d4ad09
sam_bland <sbland.co.uk@gmail.com>
2020-10-01 15:49:24 +0100

    add build targets to task file

--- taskfile.js
+++ taskfile.js
@@ -8,9 +8,11 @@ let ROOT = process.env.PWD;
 TAG = TAG || 'latest';
 PORT = PORT || 5000;
 ROOT_VOLUME = ROOT.replace(/([A-Z]):\//, "\/\/$1\/");
+LOG_LOCATION = `${ROOT}/task_logs/`
 
 const run_wrap = (fn) => (...args) => {
   if (!PROJECT_NAME) throw Error('Ensure PROJECT_NAME is set in .env file')
+  sh(`mkdir -p ${LOG_LOCATION};`)
   fn(...args);
 }
 
@@ -20,19 +22,28 @@ function hello_world(options, name="bob"){
 }
 
 
-function rundocker(options) {
-  sh(`docker run -it -p ${PORT}:5000 -v ${ROOT_VOLUME}/app/server:/app ${PROJECT_NAME}:${TAG} `);
+function docker_run(options, target) {
+  const targets = {
+    server: () => sh(`docker run -it -p ${PORT}:5000 -v ${ROOT_VOLUME}/app/server:/node/app ${PROJECT_NAME}_app_server:${TAG}  > ${LOG_LOCATION}/docker_run_app_server.log`, {async: true}),
+    client: () => sh(`docker run -it -p 3000:3000 -v ${ROOT_VOLUME}/app/client:/node/app ${PROJECT_NAME}_app_client:${TAG}  > ${LOG_LOCATION}/docker_run_app_client.log`, {async: true}),
+  }
+  if (target) targets[target]();
+  else Object.values(targets).forEach((t) => t())
+  // else Object.values(targets).forEach((t) => t());
 }
 
-function builddocker(options, mode = 'PRODUCTION', runOnComplete = false) {
-  console.log(ROOT_VOLUME)
-  console.log(`building ${PROJECT_NAME}`);
-  sh(`cd ${ROOT}/app/server && docker build -t ${PROJECT_NAME}:${TAG} .`);
-  if (runOnComplete) rundocker()
+function docker_build(options, target, mode = 'DEVELOPMENT') {
+  const targets = {
+    server: () => sh(`cd ${ROOT}/app/server && docker build -t ${PROJECT_NAME}_app_server:${TAG} . > ${LOG_LOCATION}/docker_build_app_server.log`, {async: true}),
+    client: () => sh(`cd ${ROOT}/app/client && docker build -t ${PROJECT_NAME}_app_client:${TAG} . > ${LOG_LOCATION}/docker_build_app_client.log`, {async: true}),
+  }
+  if (target) targets[target]();
+  else Object.values(targets).forEach((t) => t())
 }
 
 
 cli({
   hello_world: run_wrap(hello_world),
-  builddocker: run_wrap(builddocker),
+  docker_build: run_wrap(docker_build),
+  docker_run: run_wrap(docker_run),
 })
