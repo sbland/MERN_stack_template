7964580441a819a696329f06aca477d2473b3fd9
sam_bland <sbland.co.uk@gmail.com>
2020-10-01 15:51:04 +0100

    Add complete server docker demo setup

--- app/server/server.js
+++ app/server/server.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+const express = require('express');
+const PORT = 5000
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+});
